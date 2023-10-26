2e847ff6a80c09b7067ddfd1bb185ebd719597ba
Ezequiel <ki3lmigu3l@gmail.com>
2023-10-26 19:17:04 -0300

    Primeiro commit, iniciando o projeto da API de Vendas

---
+++ src/main/java/com/github/ki3lmigu3l/vendas/VendasApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.ki3lmigu3l.vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasApiApplication.class, args);
+	}
+
+}
